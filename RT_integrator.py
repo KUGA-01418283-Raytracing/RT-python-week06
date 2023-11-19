524a26ef95bb7a5db68e956cfcc4ce7b065e62e3
Tanaboon <darkhikaru@gmail.com>
2023-11-19 21:26:37 +0700

    Finalize week06
    - update a new scene.
    - add comments
    - reorganize codes and comments.

--- RT_integrator.py
+++ RT_integrator.py
@@ -48,6 +48,6 @@ class Integrator():
             # return the color
             return Le + self.compute_scattering(rtr.Ray(hinfo.getP(), sinfo.scattered_ray.getDirection()), scene, maxDepth-1) * sinfo.attenuation_color
 
-        # return scene.get_sky_background_color(rGen_ray)
-        return scene.getBackgroundColor()
+        return scene.get_sky_background_color(rGen_ray)
+        # return scene.getBackgroundColor()
 
--- RT_material.py
+++ RT_material.py
@@ -5,7 +5,7 @@ import math
 
 def reflect(vRay, vNormal):
     # return the perfect reflection direction
-    return vRay - vNormal*rtu.Vec3.dot_product(vRay, vNormal)*2.0
+    return vRay
 
 def refract(vRay, vNormal, fRefractRatio):
     cos_theta = min(rtu.Vec3.dot_product(-vRay, vNormal), 1.0)
@@ -52,14 +52,14 @@ class Lambertian(Material):
 class Mirror(Material):
     def __init__(self, cAlbedo) -> None:
         super().__init__()
-        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
+
 
     def scattering(self, rRayIn, hHinfo):
         # generate a reflected ray
-        reflected_ray = rtr.Ray(hHinfo.getP(), reflect(rRayIn.getDirection(), hHinfo.getNormal()))
+
 
         # get attenuation_color
-        attenuation_color = rtu.Color(self.color_albedo.r(), self.color_albedo.g(), self.color_albedo.b())
+
 
         # return scattering info
         return rtu.Scatterinfo(reflected_ray, attenuation_color)
@@ -68,19 +68,16 @@ class Mirror(Material):
 class Dielectric(Material):
     def __init__(self, cAlbedo, fIor) -> None:
         super().__init__()
-        self.color_albedo = rtu.Color(cAlbedo.r(), cAlbedo.g(), cAlbedo.b())
-        self.IOR = fIor
+
 
     def scattering(self, rRayIn, hHinfo):
-        attenuation_color = self.color_albedo
+
         refract_ratio = self.IOR
         if hHinfo.front_face:
             refract_ratio = 1.0/self.IOR
 
         # generate a refracted ray
-        uv = rtu.Vec3.unit_vector(rRayIn.getDirection())
-        refracted_dir = refract(uv, hHinfo.getNormal(), refract_ratio)
-        scattered_ray = rtr.Ray(hHinfo.getP(), refracted_dir)
+
 
         # return scattering info
         return rtu.Scatterinfo(scattered_ray, attenuation_color)
